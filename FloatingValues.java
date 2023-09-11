046d39d65055bf45793f2ebe662c11c27580751b
mckrywanio <michael.krywanio@csuglobal.edu>
2023-09-10 19:11:13 -0600

    changed messages.

--- FloatingValues.java
+++ FloatingValues.java
@@ -1,22 +1,34 @@
 //---------------------------------
-//Program Name:CriticalThinkingMod#3
+//Program Name:CriticalThinkingMod#4
 //Author:Michael C Krywanio
-//Date: SEP 3 2023
+//Date: SEP 10 2023
 //---------------------------------
 //PsudoCode
 //START
-//  INPUT weeklyIncome
-//  IF weeklyIncome < 500 THEN
-//   taxRate = 10%
-//  ELSE IF weeklyIncome >= 500 AND weeklyIncome < 1500 THEN
-//    taxRate = 15%
-//  ELSE IF weeklyIncome >= 1500 AND weeklyIncome < 2500 THEN
-//    taxRate = 20%
-//  ELSE
-//    taxRate = 30%
-//  END IF
-//  taxWithheld = weeklyIncome * taxRate
-//  OUTPUT taxWithheld
+
+//Initialize count = 0
+//Initialize total = 0.0
+//Initialize maximum as a very small number
+//Initialize minimum as a very large number
+
+//WHILE count is less than 5 DO
+//    Prompt the user to enter a floating-point value
+//    Read the value from the user
+//    Add value to total
+//    IF value is greater than maximum THEN
+//        set maximum to value
+//    ENDIF
+//    IF value is less than minimum THEN
+//        set minimum to value
+//    ENDIF
+//    Increment count
+//ENDWHILE
+
+//Compute average as total divided by 5
+//Compute interest as total multiplied by 0.20
+
+//Print total, average, maximum, minimum, interest
+
 //END
 //-----------------------------------
 //Program Inputs: Weekly Income
@@ -33,7 +45,7 @@ public class FloatingValues {
         double total = 0.0;
         double maximum = Double.MIN_VALUE; // A very small initial value
         double minimum = Double.MAX_VALUE; // A very large initial value
-
+        System.out.print("This program will compute the max, min, average, total, and interst on total at 20%.");
         while (count < 5) {
             System.out.print("Enter a floating-point value (" + (5 - count) + " left): ");
             double value = scanner.nextDouble();
